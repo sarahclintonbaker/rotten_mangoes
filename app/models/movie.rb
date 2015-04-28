d22c80de333d21e822a461376ee2ff26023ac5ba
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-04-28 20:42:01 +0000

    finished tutorial version 1.0

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -20,18 +20,22 @@ class Movie < ActiveRecord::Base
   validates :release_date,
     presence: true
 
-  validate :release_date_is_in_the_future
+  # validate :release_date_is_in_the_future
 
   def review_average
-    reviews.sum(:rating_out_of_ten)/reviews.size
+    if reviews.size == 0
+      errors.add(:rating_out_of_ten, "there are no reviews yet")
+    else
+    (reviews.sum(:rating_out_of_ten)/reviews.size)
+    end 
   end
 
   protected
 
-  def release_date_is_in_the_future
-    if release_date.present?
-      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
-    end
-  end
+  # def release_date_is_in_the_future
+  #   if release_date.present?
+  #     errors.add(:release_date, "should probably be in the future") if release_date < Date.today
+  #   end
+  # end
 
 end
