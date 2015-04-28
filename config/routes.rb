ae6643c503ffaf467ae39bf67bb190503a5092ef
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-04-28 17:59:53 +0000

    second commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+RottenMangoes::Application.routes.draw do
+
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+
+end
